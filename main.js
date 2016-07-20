4f2a6464ae58a49cf0e377a0ee22b428a4f7dd37
Victor Fung <victor.fung122@gmail.com>
2016-07-20 12:40:33 -0400

    now gets all files

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 var fs = require("fs");
-var unzip = require('unzip')
-var path = require('path')
-var mkdir = require('mkdirp')
+var unzip = require('unzip');
+var path = require('path');
+var mkdir = require('mkdirp');
+var EventEmitter=require('events').EventEmitter;
+var filesEE=new EventEmitter();
 
-var zipFileArray = ['Version 0.0.63.zip', 'Version 0.0.62.zip']
+var zipFileArray = [];
 
-zipFileArray.forEach(function(value){
-  var dirPath = './Data/';
-  var folderName = path.basename(value,'.zip')
-  fs.createReadStream(dirPath+value)
-    .pipe(unzip.Parse())
-    .on('entry', function (entry) {
-      var fileName = entry.path;
-      var type = entry.type; // 'Directory' or 'File'
-      var size = entry.size;
-      if (fileName === "importExport\\ImportExport.xml") {
-        var fullPath = __dirname + '/Data/' + folderName
-        fileName = path.basename( fileName )
-        mkdir.sync(fullPath)
-        entry.pipe(fs.createWriteStream( fullPath + '/' + fileName ))
-      }
-      else {
-        entry.autodrain();
-      }
+fs.readdir('./Data',function(err,files){
+  if(err) throw err;
+  files.forEach(function(file){
+    zipFileArray.push(file);
+  });
+  filesEE.emit('files_ready');
+});
+
+filesEE.on('files_ready',function(){
+  zipFileArray.forEach(function(value){
+    var dirPath = './Data/';
+    var folderName = path.basename(value,'.zip')
+    fs.createReadStream(dirPath+value)
+      .pipe(unzip.Parse())
+      .on('entry', function (entry) {
+        var fileName = entry.path;
+        var type = entry.type; // 'Directory' or 'File'
+        var size = entry.size;
+        if (fileName === "importExport\\ImportExport.xml") {
+          var fullPath = __dirname + '/Data/' + folderName
+          fileName = path.basename( fileName )
+          mkdir.sync(fullPath)
+          entry.pipe(fs.createWriteStream( fullPath + '/' + fileName ))
+        }
+        else {
+          entry.autodrain();
+        }
+    });
   });
 });
