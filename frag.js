73d5441fc93457a64a5f50c27cf15e9a08343765
Victor Fung <victor.fung122@gmail.com>
2016-07-25 14:56:47 -0400

    updating

--- frag.js
+++ frag.js
@@ -2,11 +2,24 @@ var fs = require("fs");
 var saxParser = require('sax').createStream(true);
 var saxPath = require('saxpath');
 var path = require('path');
+var xpath = require('xpath');
+var dom = require('xmldom').DOMParser;
 
 function getDirectories(srcpath) {
   return fs.readdirSync(srcpath).filter(function(file) {
     return fs.statSync(path.join(srcpath, file)).isDirectory();
   });
+}
+
+function checkSubDirectorySync(directory, definingType, methodname, xml) {
+  try {
+    fs.statSync(directory);
+    fs.writeFileSync('./'+store+'/'+definingType+'/'+methodname, '');
+    fs.appendFileSync('./'+store+'/'+definingType+'/'+methodname, xml, {encoding:'utf8'});
+  } catch(e) {
+    fs.mkdirSync(directory);
+    fs.appendFileSync('./'+store+'/'+definingType+'/'+methodname, xml, {encoding:'utf8'});
+  }
 }
 
 function checkDirectorySync(directory) {
@@ -17,27 +30,19 @@ function checkDirectorySync(directory) {
   }
 }
 
-checkDirectorySync("./parse");
-var searchFor = 'nameMapping';
+var store = 'IACUC';
+checkDirectorySync('./'+store);
 var folders = getDirectories('./extract');
 folders.forEach(function(value){
   var dataURL = './extract/'+value+'/ImportExport.xml';
-  var count = 0;
   var fileStream = fs.createReadStream(dataURL);
-  var streamer = new saxPath.SaXPath(saxParser, '//'+searchFor);
-  fs.exists('./parse/'+searchFor+'.xml', function(exists) {
-    if (exists) {
-      fs.writeFileSync('./parse/'+searchFor+'.xml', 0)
-      streamer.on('match', function(xml) {
-        fs.appendFileSync('./parse/'+searchFor+'.xml', xml, {encoding:'utf8'})
-      });
-      fileStream.pipe(saxParser);
-    }
-    else {
-      streamer.on('match', function(xml) {
-        fs.appendFileSync('./parse/'+searchFor+'.xml', xml, {encoding:'utf8'})
-      });;
-      fileStream.pipe(saxParser);
-    }
+  var streamer = new saxPath.SaXPath(saxParser, '//method');
+  streamer.on('match', function(xml) {
+    doc = new dom().parseFromString(xml);
+    definingType = xpath.select1("/method/@definingType", doc).value;
+    methodname = xpath.select1("/method/@methodname", doc).value;
+    console.log(methodname);
+    checkSubDirectorySync('./'+store+'/'+definingType, definingType, methodname, xml);
   });
+  fileStream.pipe(saxParser);
 });
