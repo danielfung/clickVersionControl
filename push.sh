3327a2179cfe75447e8700173aec5b1509fa140d
= <=>
2016-07-26 15:57:24 -0400

    push

--- push.sh
+++ push.sh
@@ -1,6 +1,4 @@
-git config user.email = "victor.fung122@gmail.com"
-git config user.name = "VictorFung1"
-git remote set-url origin git@github.com:VictorFung1/clickVersionControl.git
-git add --all
-git commit -m "message"
+commit_message="$1"
+git add . -A
+git commit -m "$commit_message"
 git push
