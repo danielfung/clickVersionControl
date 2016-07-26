3585821da82d29ea3b9b688b84edf7b99855e238
= <=>
2016-07-26 15:39:14 -0400

    adding test file

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
+git config user.email = "victor.fung122@gmail.com"
+git config user.name = "VictorFung1"
+git remote set-url origin git@github.com:VictorFung1/clickVersionControl.git
 git add --all
-git commit -m "adding new comments"
+git commit -m "message"
 git push
-
-#random new comments
